a27a5853e6286b49e19c999f9fed289c678de723
johnviti <jvsa5@aluno.ifal.edu.br>
2024-05-12 21:28:34 -0300

    Feat: Desafio do contador

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Contador{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int parametroUm;
+        int parametroDois;
+        System.out.println("Digite o primeiro parâmetro");
+        parametroUm = scanner.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        parametroDois = scanner.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosExceptio e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosExceptio {
+
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosExceptio("O segundo parâmetro deve ser maior que o primeiro");
+        }
+        int contador = parametroDois - parametroUm;
+        for (int i = 1; i <= contador; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
---
+++ DesafioControleFluxo/src/ParametrosInvalidosExceptio.java
@@ -0,0 +1,11 @@
+public class ParametrosInvalidosExceptio extends Exception {
+    
+    public ParametrosInvalidosExceptio(String message) {
+        super(message);
+    }
+    
+    public ParametrosInvalidosExceptio(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
